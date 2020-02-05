742768a9e4b9be2ed06965c68f97fec621d267da
unknown <valdes.ruben92@gmail.com>
2020-02-05 00:49:07 -0300

    Uploading frontend to heroku

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
     
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://warm-dawn-80266.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
